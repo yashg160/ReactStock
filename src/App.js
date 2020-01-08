e090449c3af2760aa315b2767daeccb7d5e7afbb
Yash Gupta <yashg160@gmail.com>
2020-01-08 16:11:51 +0530

    Initial Setup and Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './screens/Home';
+
+export default function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Home} />
+			</Switch>
+		</Router>
+	)
 }
-
-export default App;
