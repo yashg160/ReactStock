4213074e3486bdeae38c8a69fb467703630c6913
Yash Gupta <yashg160@gmail.com>
2020-01-22 22:34:17 +0530

    Added user/ Route for Flutter App

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,145 +6,191 @@ var Picture = require('../models/Picture');
 var userRouter = express.Router();
 userRouter.use(bodyParser.json());
 
-userRouter.route('/login')
-    .get((req, res, next) => {
-        var query = req.query
+userRouter.route('/').get((req, res, next) => {
+	var query = req.query;
+	console.log(req.query);
 
-        User.findOne({
-            accessToken: query.accessToken
-        }, (err, success) => {
-                if (err) {
-                    res.status(200);
-                    res.statusMessage = 'ERR_FIND_USER';
-                    res.send({
-                        error: true,
-                        errorMessage: 'ERR_FIND_USER'
-                    });
-                }
-                else {
-                    if (success == null) {
-                        res.status(200);
-                        res.statusMessage = 'ERR_TOKEN';
-                        res.send({
-                            error: true,
-                            errorMessage: 'ERR_TOKEN'
-                        });
-                    }
-                    else {
-                        res.status(200);
-                        res.statusMessage = 'ERR_NONE';
-                        res.send({
-                            error: false,
-                            errorMessage: 'ERR_NONE',
-                            accessToken: query.accessToken,
-                            profile: {
-                                givenName: success.get('name').given,
-                                familyName: success.get('name').family,
-                                imageUrl: success.get('imageUrl'),
-                                email: success.get('email'),
-                                pictures: success.get('pictures')
-                            }
-                        });
-                    }
-                }
-        })
-    })
-    .post((req, res, next) => {
-        var body = req.body
+	User.findOne(
+		{
+			email: query.email
+		},
+		(err, success) => {
+			if (err) {
+				res.status(200);
+				res.statusMessage = 'ERR_FIND_USER';
+				res.send({
+					error: true,
+					errorMessage: 'ERR_FIND_USER'
+				});
+			} else {
+				if (success == null) {
+					res.status(200);
+					res.statusMessage = 'ERR_EMAIL';
+					res.send({
+						error: true,
+						errorMessage: 'ERR_EMAIL'
+					});
+				} else {
+					res.status(200);
+					res.statusMessage = 'ERR_NONE';
+					res.send({
+						error: false,
+						errorMessage: 'ERR_NONE',
+						profile: {
+							givenName: success.get('name').given,
+							familyName: success.get('name').family,
+							imageUrl: success.get('imageUrl'),
+							email: success.get('email'),
+							pictures: success.get('pictures')
+						}
+					});
+				}
+			}
+		}
+	);
+});
+userRouter
+	.route('/login')
+	.get((req, res, next) => {
+		var query = req.query;
 
-        User.findOne({
-            googleId: body.googleId
-        },
-            (error, success) => {
-                if (error) {
-                    res.status(200);
-                    res.statusMessage = 'ERR_LOGIN'
-                    res.send({ error: true });
-                }
-                else {
-                    console.log(success);
-                    if (success == null) {
-                        User.create(body, (err, createdSuccess) => {
-                            if (err) {
-                                res.status(200);
-                                res.statusMessage = 'ERR_LOGIN'
-                                res.send({ error: true });
-                            }
-                            else {
-                                console.log(createdSuccess);
-                                res.status(200);
-                                res.statusMessage = 'ERR_NONE';
-                                res.send({
-                                    error: false,
-                                    accessToken: body.accessToken,
-                                    userCreated: true
-                                });
-                            }
-                        });
-                    }
-                    else {
-                        console.log(success);
-                        User.updateOne({
-                            googleId: body.googleId
-                        }, {
-                            accessToken: body.accessToken
-                        }, (err, updateSuccess) => {
-                            if (err) {
-                                res.status(200);
-                                res.statusMessage = 'ERR_UPDATE_TOKEN'
-                                res.send({ error: true });
-                            }
-                            else {
-                                console.log(updateSuccess);
-                                res.status(200);
-                                res.statusMessage = 'ERR_NONE';
-                                res.send({
-                                    error: false,
-                                    accessToken: body.accessToken,
-                                    userCreated: false
-                                });
-                            }
-                        });
-                        
-                    }
-                }
-            });
-    });
+		User.findOne(
+			{
+				accessToken: query.accessToken
+			},
+			(err, success) => {
+				if (err) {
+					res.status(200);
+					res.statusMessage = 'ERR_FIND_USER';
+					res.send({
+						error: true,
+						errorMessage: 'ERR_FIND_USER'
+					});
+				} else {
+					if (success == null) {
+						res.status(200);
+						res.statusMessage = 'ERR_TOKEN';
+						res.send({
+							error: true,
+							errorMessage: 'ERR_TOKEN'
+						});
+					} else {
+						res.status(200);
+						res.statusMessage = 'ERR_NONE';
+						res.send({
+							error: false,
+							errorMessage: 'ERR_NONE',
+							accessToken: query.accessToken,
+							profile: {
+								givenName: success.get('name').given,
+								familyName: success.get('name').family,
+								imageUrl: success.get('imageUrl'),
+								email: success.get('email'),
+								pictures: success.get('pictures')
+							}
+						});
+					}
+				}
+			}
+		);
+	})
+	.post((req, res, next) => {
+		var body = req.body;
 
-userRouter.route('/pictures')
-    .get((req, res, next) => {
-        var query = req.query;
+		User.findOne(
+			{
+				googleId: body.googleId
+			},
+			(error, success) => {
+				if (error) {
+					res.status(200);
+					res.statusMessage = 'ERR_LOGIN';
+					res.send({ error: true });
+				} else {
+					console.log(success);
+					if (success == null) {
+						User.create(body, (err, createdSuccess) => {
+							if (err) {
+								res.status(200);
+								res.statusMessage = 'ERR_LOGIN';
+								res.send({ error: true });
+							} else {
+								console.log(createdSuccess);
+								res.status(200);
+								res.statusMessage = 'ERR_NONE';
+								res.send({
+									error: false,
+									accessToken: body.accessToken,
+									userCreated: true
+								});
+							}
+						});
+					} else {
+						console.log(success);
+						User.updateOne(
+							{
+								googleId: body.googleId
+							},
+							{
+								accessToken: body.accessToken
+							},
+							(err, updateSuccess) => {
+								if (err) {
+									res.status(200);
+									res.statusMessage = 'ERR_UPDATE_TOKEN';
+									res.send({ error: true });
+								} else {
+									console.log(updateSuccess);
+									res.status(200);
+									res.statusMessage = 'ERR_NONE';
+									res.send({
+										error: false,
+										accessToken: body.accessToken,
+										userCreated: false
+									});
+								}
+							}
+						);
+					}
+				}
+			}
+		);
+	});
 
-        User.findOne({
-            accessToken: query.accessToken
-        }, async(err, doc) => {
-                if (err) {
-                    res.status(200);
-                    res.send({ error: true, errorMessage: 'ERR_USER_PICTURES' });
-                }
-                else {
-                    var pictureIds = doc.get('pictures');
-                    let pictures = [];
+userRouter.route('/pictures').get((req, res, next) => {
+	var query = req.query;
 
-                    for (i = 0; i < pictureIds.length; i++) {
+	User.findOne(
+		{
+			accessToken: query.accessToken
+		},
+		async (err, doc) => {
+			if (err) {
+				res.status(200);
+				res.send({ error: true, errorMessage: 'ERR_USER_PICTURES' });
+			} else {
+				var pictureIds = doc.get('pictures');
+				let pictures = [];
 
-                        let pic = await Picture.findById(pictureIds[i]);
+				for (i = 0; i < pictureIds.length; i++) {
+					let pic = await Picture.findById(pictureIds[i]);
 
-                        pictures.push({
-                            id: pictureIds[i],
-                            content: pic.get('content'),
-                            title: pic.get('title')
-                        });                      
-                    }
+					pictures.push({
+						id: pictureIds[i],
+						content: pic.get('content'),
+						title: pic.get('title')
+					});
+				}
 
-                    res.status(200);
-                    res.send({
-                        error: false,
-                        errorMessage: 'ERR_NONE',
-                        pictures: pictures
-                    });
-                }
-        })
-    });
+				res.status(200);
+				res.send({
+					error: false,
+					errorMessage: 'ERR_NONE',
+					pictures: pictures
+				});
+			}
+		}
+	);
+});
 
 module.exports = userRouter;
