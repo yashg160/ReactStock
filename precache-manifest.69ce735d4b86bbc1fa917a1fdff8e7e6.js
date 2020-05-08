4c5bd68d10ae29f939e79adb26592e8f06f80a0d
yashg160 <yashg160@gmail.com>
2020-05-08 17:49:03 +0530

    Updates

---
+++ precache-manifest.69ce735d4b86bbc1fa917a1fdff8e7e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522b0f4742824dc61b7f6a228aba6b01",
+    "url": "/ReactStock/index.html"
+  },
+  {
+    "revision": "8ae3e30bb4b93977043c",
+    "url": "/ReactStock/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "452992ba40d9d199a485",
+    "url": "/ReactStock/static/js/2.cc15a6e4.chunk.js"
+  },
+  {
+    "revision": "cf48389422cda2513b8eee86ab8b1ab6",
+    "url": "/ReactStock/static/js/2.cc15a6e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ae3e30bb4b93977043c",
+    "url": "/ReactStock/static/js/main.a5365d0d.chunk.js"
+  },
+  {
+    "revision": "0ea876b879be98b30981",
+    "url": "/ReactStock/static/js/runtime-main.5ef5a458.js"
+  },
+  {
+    "revision": "dbe5ad592b9e28e30326c03b1832ac95",
+    "url": "/ReactStock/static/media/home-background.dbe5ad59.png"
+  }
+]);
